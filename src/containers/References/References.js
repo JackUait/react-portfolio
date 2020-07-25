0bf800a61dbd3ec524238a36726f96047bc7fe55
JackUait <jackuait@bk.ru>
2020-07-25 14:34:59 +0300

    font fixes

--- src/containers/References/References.js
+++ src/containers/References/References.js
@@ -7,27 +7,28 @@ import Quotes from "../../components/Quotes/Quotes";
 const References = () => {
 	const [quotes] = useState([
 		{
-			quote: "A genuine, compassionate friend who exhibits programming skills that excel beyond expectation, reflecting his incredibly driven nature.",
+			quote:
+				"A genuine, compassionate friend who exhibits programming skills that excel beyond expectation, reflecting his incredibly driven nature.",
 			author: "Alexa Bachurski",
-			activity: "The best homie ever",
+			activity: "Software Engineer from Pittsburgh, USA",
 			id: 1,
 		},
 		{
-			quote: "He's doing his best to make his programs work as they supposed to",
+			quote:
+				"He's doing his best to make his programs work as they supposed to",
 			author: "Aleksey Berezhnoy",
 			activity: "Genius Software Engineer",
 			id: 2,
 		},
 		{
-			quote: "If you are reading this you are really interested what people think about me. Well, I'm interested too, butt",
-			author: "John Doe",
-			activity: "He's not real",
+			quote:
+				"He's a really good at school computer science. To be honest I haven't tested him anywhere else",
+			author: "Irina Skorohodova",
+			activity: "Writer, actress, schoolgirl, unemployed",
 			id: 3,
 		},
 	]);
 	let [currentQuote, setCurrentQuote] = useState(0);
-
-	
 
 	const body = [];
 	for (let i = 0; i < quotes.length; i++) {
@@ -37,57 +38,52 @@ const References = () => {
 				key={i}
 				id={i}
 				onClick={() => setCurrentQuote(i)}
-				style={{width: currentQuote === i ? 70 : 15}}
+				style={{ width: currentQuote === i ? 70 : 15 }}
 			></li>,
 		);
 	}
 	useEffect(() => {
 		let counter = 0;
 
-		let changeTimer = setTimeout(function tick () {
-			if (counter === quotes.length -1) {
+		let changeTimer = setTimeout(function tick() {
+			if (counter === quotes.length - 1) {
 				counter = 0;
 				setCurrentQuote(0);
 			} else {
 				counter++;
-				setCurrentQuote(prev => prev + 1);
+				setCurrentQuote((prev) => prev + 1);
 			}
-			changeTimer = setTimeout(tick,20000);
-		})
+			changeTimer = setTimeout(tick, 15000);
+		});
 
-		document.addEventListener('keydown', (event) => {
-			if (event.key === 'ArrowRight') {
-				if (counter < quotes.length-1) {
+		document.addEventListener("keydown", (event) => {
+			if (event.key === "ArrowRight") {
+				if (counter < quotes.length - 1) {
 					counter++;
-					setCurrentQuote(prev => prev + 1);
+					setCurrentQuote((prev) => prev + 1);
 				} else {
 					counter = 0;
 					setCurrentQuote(0);
-				};	
-				
-			}
-			if (event.key === 'ArrowLeft') {
-				if (counter === 0) {
-					counter = quotes.length-1;
-					setCurrentQuote(quotes.length-1)
-				} else {
-					counter--;
-					setCurrentQuote(prev => prev -1);
-
 				}
 			}
-		})
-	}, [])
-	
+			if (event.key === "ArrowLeft") {
+				if (counter === 0) {
+					counter = quotes.length - 1;
+					setCurrentQuote(quotes.length - 1);
+				} else {
+					counter--;
+					setCurrentQuote((prev) => prev - 1);
+				}
+			}
+		});
+	}, []);
+
 	console.log(currentQuote);
 	return (
 		<div className={classes.wrapper}>
 			<HeaderText color="#fff">References</HeaderText>
 			<SubHeader>who says, what says</SubHeader>
-			<Quotes
-				quotes={quotes}
-				currentQuote={currentQuote}
-			/>
+			<Quotes quotes={quotes} currentQuote={currentQuote} />
 			<ul className={classes.switcher}>{body}</ul>
 		</div>
 	);
